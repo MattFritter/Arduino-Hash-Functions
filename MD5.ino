5718beb3312a066a4da24d40c3dd2b1cc6fe8d9e
Matt Fritter <mattfritter@gmail.com>
2017-12-01 23:56:57 -0800

    Big bugfix and optimization
    Fixed a major bug in the block addressing that caused incorrect hashes when for inputs of more than one 512-bit padded block. Moved constants to program memory and changed some datatypes to save memory. Code now runs with a 512 byte input (up from ~240 bytes previously) without crashing.

--- MD5.ino
+++ MD5.ino
@@ -7,6 +7,12 @@
  * 128 bits.
  * Author: Matthew Fritter
  */
+/* Include the ProgMem library */
+#include <avr/pgmspace.h>
+
+/* Constants (round offsets and constants), stored in the Program Memory to save dynamic memory */
+const uint32_t sine[] PROGMEM = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
+const byte offset[] PROGMEM = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};   
 
 /* Define the rotation function */
 #define rot(x, c) ((x << c) | ( x >> (32 - c)))
@@ -20,9 +26,9 @@
  * Final output is in the form of four unsigned 32 bit integers, for a total of 128 bits. These are
  * placed in a uint32_t array of size four, initialized and passed to the function by the caller.
  */
-void hashMD5(byte* message, int arraySize, uint32_t* outArray) {
+void hashMD5(uint8_t* message, size_t arraySize, uint32_t* outArray) {
   uint32_t len = (arraySize << 3) + 1;
-  uint64_t sizeBit = len - 1;
+  uint32_t sizeBit = len - 1;
   if((len % 512) > 448){
     len += 512 - (len % 512) + 448;
   }else if((len % 512) < 448){
@@ -33,19 +39,18 @@ void hashMD5(byte* message, int arraySize, uint32_t* outArray) {
   memset(newArr, 0, len);
   memcpy(newArr, message, arraySize);
   newArr[arraySize] = 128;
-  memcpy(newArr + (len-8), &sizeBit, 8);
+  memcpy(newArr + (len-8), &sizeBit, 4);
   outArray[0] = 0x67452301;
   outArray[1] = 0xefcdab89;
   outArray[2] = 0x98badcfe;
   outArray[3] = 0x10325476;
   while(len != 0){
+    uint32_t *block = (uint32_t *) (newArr + ((sizeof(newArr)) - len));
     uint32_t A = outArray[0];
     uint32_t B = outArray[1];
     uint32_t C = outArray[2];
     uint32_t D = outArray[3];
     for(int i = 0; i < 64; i++){
-      uint32_t k[] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
-      uint32_t r[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
       uint32_t F, g;
       if((0 <= i) && (i <= 15)){
         F = (B & C) | ((~B) & D);
@@ -60,13 +65,11 @@ void hashMD5(byte* message, int arraySize, uint32_t* outArray) {
         F = C ^ (B | (~D));
         g = (7 * i) % 16;
       }
-      uint32_t block;
-      memcpy(&block, &newArr[(len-64) + (g * 4)], 4);
-      F = F + A + k[i] + block;
+      F = F + A + pgm_read_dword(sine + i) + block[g];
       A = D;
       D = C;
       C = B;
-      B = B + rot(F, r[i]);
+      B = B + rot(F, pgm_read_byte_near(offset + i));
     }
     outArray[0] += A;
     outArray[1] += B;
@@ -87,7 +90,7 @@ void setup(){
  * properly implemented.
  */
 void loop(){
-  char message[] = "The quick brown fox jumps over the lazy dog";
+  uint8_t message[] = "The quick brown fox jumps over the lazy dog";
   size_t arraySize = sizeof(message)-1;
   uint32_t outArray[4] = {0};
   hashMD5(message, arraySize, outArray);
@@ -97,4 +100,3 @@ void loop(){
   sprintf(out, "%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X", hexb[0], hexb[1], hexb[2], hexb[3], hexb[4], hexb[5], hexb[6], hexb[7], hexb[8], hexb[9], hexb[10], hexb[11], hexb[12], hexb[13], hexb[14], hexb[15]);
   Serial.println(out);
 }
-
